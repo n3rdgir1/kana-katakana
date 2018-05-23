a220bd5c71e80515e6bc15d37e5d8b31285df5f9
n3rdgir1 <nerdgirl11235813@gmail.com>
2018-05-23 14:33:41 -0500

    highlights active nav item

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink as Link } from 'react-router-dom';
 import Home from './Home';
 import Hiragana from './Hiragana';
 import Katakana from './Katakana';
@@ -14,8 +14,8 @@ class App extends Component {
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">Kanas</a>
             <div className="navbar" id="navbarNav">
-              <Link to='/katakana' className="nav-item nav-link"> Katakana </Link>
-              <Link to='/hiragana' className="nav-item nav-link"> Hiragana </Link>
+              <Link to='/katakana' className="nav-item nav-link" activeClassName='active'> Katakana </Link>
+              <Link to='/hiragana' className="nav-item nav-link" activeClassName='active'> Hiragana </Link>
             </div>
           </nav>
 
